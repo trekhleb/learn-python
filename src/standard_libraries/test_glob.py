2e82640d52990895df4195dd91fe27e57d39548a
Oleksii Trekhleb <trehleb@gmail.com>
2018-09-05 16:07:32 +0300

    Add comments to the blob section.

--- src/standard_libraries/test_glob.py
+++ src/standard_libraries/test_glob.py
@@ -11,6 +11,10 @@ import glob
 def test_glob():
     """File Wildcards."""
 
+    # == operator for lists relies on the order of elements in the list.
+    # In some cases (like on Linux Mint, python3.6) the glob() function returns list
+    # in reverse order then  it might be expected. Thus lets sort both lists before comparison
+    # using sorted() built-in function.
     assert sorted(glob.glob('src/standard_libraries/glob_files/*.txt')) == sorted([
         'src/standard_libraries/glob_files/first_file.txt',
         'src/standard_libraries/glob_files/second_file.txt'
