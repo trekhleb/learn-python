f954f0b43e1a25e62cc1b35944793bd0a9867d71
Christian Clauss <cclauss@me.com>
2021-07-20 12:22:55 +0200

    Fix typo discovered by codespell

--- src/getting_started/test_variables.py
+++ src/getting_started/test_variables.py
@@ -18,7 +18,7 @@ A variable can have a short name (like x and y) or a more descriptive name
 Rules for Python variables:
 - A variable name must start with a letter or the underscore character.
 - A variable name cannot start with a number.
-- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+- A variable name can only contain alphanumeric characters and underscores (A-z, 0-9, and _ ).
 - Variable names are case-sensitive (age, Age and AGE are three different variables).
 """
 
